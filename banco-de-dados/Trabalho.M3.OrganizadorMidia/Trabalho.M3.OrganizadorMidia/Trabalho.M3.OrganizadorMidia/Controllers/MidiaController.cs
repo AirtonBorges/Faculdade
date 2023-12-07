3c6f2b9fe94b40127c428f23989d524beff2e915
Arto <myemail>
2023-12-07 00:29:44 -0300

    feat: iniciado trabalho banco dados

---
+++ banco-de-dados/Trabalho.M3.OrganizadorMidia/Trabalho.M3.OrganizadorMidia/Trabalho.M3.OrganizadorMidia/Controllers/MidiaController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trabalho.M3.OrganizadorMidia.Entites;
+
+namespace Trabalho.M3.OrganizadorMidia.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MidiaController : ControllerBase
+{
+    public MidiaController()
+    {
+    }
+
+    [HttpPost]
+    public ActionResult<int> Adicionar(Entites.Midia pMidia)
+    {
+        return Created(nameof(Entites.Midia), pMidia);
+    }
+
+    [HttpGet]
+    public List<Entites.Midia> Obter()
+    {
+        return new List<Entites.Midia>();
+    }
+
+    [HttpPut("{pId}")]
+    public ActionResult Editar(int pId, [FromBody]Entites.Midia pMidia)
+    {
+        if (pId != pMidia.Id)
+        {
+            return BadRequest();
+        }
+
+        return Ok();
+    }
+
+    [HttpDelete("{pId}")]
+    public ActionResult Delete(int pId)
+    {
+        return Delete(pId);
+    }
+}
---
+++ banco-de-dados/Trabalho.M3.OrganizadorMidia/Trabalho.M3.OrganizadorMidia/Trabalho.M3.OrganizadorMidia/Entites/Midia.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Trabalho.M3.OrganizadorMidia.Entites;
+
+public class Midia
+{
+    [JsonIgnore]
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public string Sinopse { get; set; }
+    public DateTime DataLancamento { get; set; }
+    public string Plataforma { get; set; }
+    public string Url { get; set; }
+}
